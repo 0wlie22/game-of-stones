e927e28aba1d03177df572a4a98928c3f48b215a
Colorato291 <regnars.slapins@gmail.com>
2024-03-17 21:00:46 +0200

    refactor: combined similar functions

--- tree/tree_utils.py
+++ tree/tree_utils.py
@@ -2,11 +2,13 @@ def generateGameState(stones, player=1, player1_points=0, player2_points=0):
     # I will assume that I get no issues with this (I will)
     return player*10**8+player1_points*10**5+stones*10**3+player2_points
 
-def getPlayer1Points(gameState): # Pirmā spēlētāja punktu skaits
-    return (gameState//10**5)%1000
-
-def getPlayer2Points(gameState): # Otrā spēlētāja punktu skaits
-    return gameState%1000
+def getPlayerPoints(gameState, player): # Pirmā spēlētāja punktu skaits
+    if player not in (1,2):
+        raise ValueError("Invalid player number! (1 or 2)")
+    if player == 1:
+        return (gameState//10**5)%1000
+    else:
+        return gameState%1000
 
 def getStones(gameState): # Atlikušo akmentiņu skaits
     return (gameState//10**3)%100
@@ -16,17 +18,19 @@ def getPlayer(gameState): # Spēlētāja kārtas Nr.
 
 # === Taking ===
 
-def take2stones(gameState): # Logic for taking two stones
+def takeStones(gameState, stoneCount): # Logic for taking two stones
+    if stoneCount not in (2,3):
+        raise ValueError("Invalid stone take ammount! (2 or 3)")
     stone_count = getStones(gameState)
-    if stone_count < 2: # Check if there are enough stones to take
+    if stone_count < stoneCount: # Check if there are enough stones to take
         return 
     player = getPlayer(gameState)
-    player1_points = getPlayer1Points(gameState)
-    player2_points = getPlayer2Points(gameState)
+    player1_points = getPlayerPoints(gameState, 1)
+    player2_points = getPlayerPoints(gameState, 2)
     if player == 1: # If it's player 1's turn
         # Point arithmetic
-        player1_points += 2
-        stone_count -= 2
+        player1_points += stoneCount
+        stone_count -= stoneCount
         # Rule about additional points depending on remaining stone count
         if stone_count % 2 == 0:
             player2_points += 2
@@ -34,36 +38,8 @@ def take2stones(gameState): # Logic for taking two stones
             player1_points +=2
     else: # 2nd player's turn
         # Point arithmetic
-        stone_count -= 2
-        player2_points += 2
-        # Rule about additional points depending on remaining stone count
-        if stone_count % 2 == 0:
-            player1_points += 2
-        else:
-            player2_points += 2
-    next_player = (player%2)+1
-    return generateGameState(stone_count, next_player, player1_points, player2_points)
-
-def take3stones(gameState): # Logic for taking two stones
-    stone_count = getStones(gameState)
-    if stone_count < 3: # Check if there are enough stones to take
-        return
-    player = getPlayer(gameState)
-    player1_points = getPlayer1Points(gameState)
-    player2_points = getPlayer2Points(gameState)
-    if player == 1: # If it's player 1's turn
-        # Point arithmetic
-        player1_points += 3
-        stone_count -= 3
-        # Rule about additional points depending on remaining stone count
-        if stone_count % 2 == 0:
-            player2_points += 2
-        else:
-            player1_points += 2
-    else: # 2nd player's turn
-        # Point arithmetic
-        stone_count -= 3
-        player2_points += 3
+        stone_count -= stoneCount
+        player2_points += stoneCount
         # Rule about additional points depending on remaining stone count
         if stone_count % 2 == 0:
             player1_points += 2
@@ -73,16 +49,18 @@ def take3stones(gameState): # Logic for taking two stones
     return generateGameState(stone_count, next_player, player1_points, player2_points)
 
 def nextGameStates(gameState):
-    result = [take2stones(gameState),take3stones(gameState)]
+    result = [takeStones(gameState,2),takeStones(gameState,3)]
     filtered_result = [state for state in result if state is not None]
     return filtered_result
  
 # === Undoing moves ===
 
-def undo_take2stones(gameState):
+def undo_takeStones(gameState, stoneCount):
+    if stoneCount not in (2,3):
+        raise ValueError("Invalid stone take ammount! (2 or 3)")
     # Value gathering
-    player1_points = getPlayer1Points(gameState) 
-    player2_points = getPlayer2Points(gameState) 
+    player1_points = getPlayerPoints(gameState, 1) 
+    player2_points = getPlayerPoints(gameState, 2) 
     player = getPlayer(gameState)
     stone_count = getStones(gameState)
     # If it is currently the 1st player's turn (means that previous was 2nd player)
@@ -91,52 +69,24 @@ def undo_take2stones(gameState):
             player1_points -= 2
         else:
             player2_points -= 2
-        player2_points -= 2
+        player2_points -= stoneCount
         # Player points have to be positive values after returning them
         if player1_points >=0 and player2_points >=0:
-            stone_count += 2
+            stone_count += stoneCount
     else: # 2nd player's turn
         if stone_count % 2 == 0:
             player2_points -= 2
         else:
             player1_points -= 2
-        player1_points -= 2
+        player1_points -= stoneCount
         # Pārbaude vai šāds stāvoklis var pastāvēt
         if player2_points >=0 and player1_points >=0:
-            stone_count += 2
-    previous_player = (player%2)+1
-    return generateGameState(stone_count, previous_player, player1_points, player2_points)
-
-def undo_take3stones(gameState):
-    # Value gathering
-    player1_points = getPlayer1Points(gameState) 
-    player2_points = getPlayer2Points(gameState) 
-    player = getPlayer(gameState)
-    stone_count = getStones(gameState)
-    # If it is currently the 1st player's turn (means that previous was 2nd player)
-    if player == 1:
-        if stone_count % 2 == 0:
-            player1_points -= 2
-        else:
-            player2_points -= 2
-        player2_points -= 3
-        # Player points have to be positive values after returning them
-        if player1_points >=0 and player2_points >=0:
-            stone_count += 3
-    else: # 2nd player's turn
-        if stone_count % 2 == 0:
-            player2_points -= 2
-        else:
-            player1_points -= 2
-        player1_points -= 3
-        # Pārbaude vai šāds stāvoklis var pastāvēt
-        if player2_points >=0 and player1_points >=0:
-            stone_count += 3
+            stone_count += stoneCount
     previous_player = (player%2)+1
     return generateGameState(stone_count, previous_player, player1_points, player2_points)
 
 def previousStates(gameState, tree):
-    result = [undo_take2stones(gameState),undo_take3stones(gameState)]
+    result = [undo_takeStones(gameState, 2),undo_takeStones(gameState, 3)]
     none_filtering = [state for state in result if state is not None]
     # Filtering
     if len(none_filtering)==0: # No previous states exist
