fd99a3a9f16df8213203bb655c01eff8e0410ec6
Regnars Slapins <regnars.slapins@gmail.com>
2024-03-21 14:49:54 +0200

    restyle: functions

--- tree/tree.py
+++ tree/tree.py
@@ -1,25 +1,22 @@
-from .tree_utils import (
-    nextGameStates,
-    previousStates,
-    generateTree
-)
+from .tree_utils import nextGameStates, previousStates, generateTree
 
-class gameTree:
+
+class GameTree:
     def __init__(self, starting_state, height=-1):
         self.levels = generateTree(starting_state, height)
-    
-    def findParent(self, game_state):
+
+    def find_parent(self, game_state):
         return previousStates(game_state, self.levels)
-    
-    def findChildren(self, game_state):
+
+    def find_children(self, game_state):
         for level in self.levels:
             if game_state in level:
                 return nextGameStates(game_state)
         print("Game state is not in tree.")
         return []
 
-    def getLevel(self, level):
+    def get_level(self, level):
         return self.levels[level]
 
-    def printAllLevels(self): # Debug purposes only
+    def print_all_levels(self):  # Debug purposes only
         print(self.levels)
--- tree/tree_utils.py
+++ tree/tree_utils.py
@@ -1,33 +1,38 @@
-def generateGameState(stones, player=1, player1_points=0, player2_points=0):
+def generate_game_state(stones, player=1, player1_points=0, player2_points=0):
     # I will assume that I get no issues with this (I will)
-    return player*10**8+player1_points*10**5+stones*10**3+player2_points
+    return player * 10**8 + player1_points * 10**5 + stones * 10**3 + player2_points
 
-def getPlayerPoints(gameState, player): # Pirmā spēlētāja punktu skaits
-    if player not in (1,2):
+
+def get_player_points(gameState, player):  # Pirmā spēlētāja punktu skaits
+    if player not in (1, 2):
         raise ValueError("Invalid player number! (1 or 2)")
     if player == 1:
-        return (gameState//10**5)%1000
+        return (gameState // 10**5) % 1000
     else:
-        return gameState%1000
+        return gameState % 1000
 
-def getStones(gameState): # Atlikušo akmentiņu skaits
-    return (gameState//10**3)%100
 
-def getPlayer(gameState): # Spēlētāja kārtas Nr.
-    return gameState//10**8
+def get_stones(gameState):  # Atlikušo akmentiņu skaits
+    return (gameState // 10**3) % 100
+
+
+def get_player(gameState):  # Spēlētāja kārtas Nr.
+    return gameState // 10**8
+
 
 # === Taking ===
 
-def takeStones(gameState, stoneCount): # Logic for taking two stones
-    if stoneCount not in (2,3):
+
+def take_stones(gameState, stoneCount):  # Logic for taking two stones
+    if stoneCount not in (2, 3):
         raise ValueError("Invalid stone take ammount! (2 or 3)")
-    stone_count = getStones(gameState)
-    if stone_count < stoneCount: # Check if there are enough stones to take
-        return 
-    player = getPlayer(gameState)
-    player1_points = getPlayerPoints(gameState, 1)
-    player2_points = getPlayerPoints(gameState, 2)
-    if player == 1: # If it's player 1's turn
+    stone_count = get_stones(gameState)
+    if stone_count < stoneCount:  # Check if there are enough stones to take
+        return
+    player = get_player(gameState)
+    player1_points = get_player_points(gameState, 1)
+    player2_points = get_player_points(gameState, 2)
+    if player == 1:  # If it's player 1's turn
         # Point arithmetic
         player1_points += stoneCount
         stone_count -= stoneCount
@@ -35,8 +40,8 @@ def takeStones(gameState, stoneCount): # Logic for taking two stones
         if stone_count % 2 == 0:
             player2_points += 2
         else:
-            player1_points +=2
-    else: # 2nd player's turn
+            player1_points += 2
+    else:  # 2nd player's turn
         # Point arithmetic
         stone_count -= stoneCount
         player2_points += stoneCount
@@ -45,25 +50,28 @@ def takeStones(gameState, stoneCount): # Logic for taking two stones
             player1_points += 2
         else:
             player2_points += 2
-    next_player = (player%2)+1
-    return generateGameState(stone_count, next_player, player1_points, player2_points)
+    next_player = (player % 2) + 1
+    return generate_game_state(stone_count, next_player, player1_points, player2_points)
 
-def nextGameStates(gameState):
-    result = [takeStones(gameState,2),takeStones(gameState,3)]
+
+def next_game_states(gameState):
+    result = [take_stones(gameState, 2), take_stones(gameState, 3)]
     filtered_result = [state for state in result if state is not None]
     return filtered_result
- 
+
+
 # === Undoing moves ===
 
-def undo_takeStones(gameState, stoneCount):
-    if stoneCount not in (2,3):
+
+def undo_take_stones(gameState, stoneCount):
+    if stoneCount not in (2, 3):
         raise ValueError("Invalid stone take ammount! (2 or 3)")
     # Value gathering
-    player1_points = getPlayerPoints(gameState, 1) 
-    player2_points = getPlayerPoints(gameState, 2) 
-    player = getPlayer(gameState)
-    stone_count = getStones(gameState)
-    # If it is currently the 1st player's turn (means that previous was 2nd player)
+    player1_points = get_player_points(gameState, 1)
+    player2_points = get_player_points(gameState, 2)
+    player = get_player(gameState)
+    stone_count = get_stones(gameState)
+    # If it is currently the 1st player's turn
     if player == 1:
         if stone_count % 2 == 0:
             player1_points -= 2
@@ -71,46 +79,50 @@ def undo_takeStones(gameState, stoneCount):
             player2_points -= 2
         player2_points -= stoneCount
         # Player points have to be positive values after returning them
-        if player1_points >=0 and player2_points >=0:
+        if player1_points >= 0 and player2_points >= 0:
             stone_count += stoneCount
-    else: # 2nd player's turn
+    else:  # 2nd player's turn
         if stone_count % 2 == 0:
             player2_points -= 2
         else:
             player1_points -= 2
         player1_points -= stoneCount
         # Pārbaude vai šāds stāvoklis var pastāvēt
-        if player2_points >=0 and player1_points >=0:
+        if player2_points >= 0 and player1_points >= 0:
             stone_count += stoneCount
-    previous_player = (player%2)+1
-    return generateGameState(stone_count, previous_player, player1_points, player2_points)
+    previous_player = (player % 2) + 1
+    return generate_game_state(
+        stone_count, previous_player, player1_points, player2_points
+    )
 
-def previousStates(gameState, tree):
-    result = [undo_takeStones(gameState, 2),undo_takeStones(gameState, 3)]
+
+def previous_states(gameState, tree):
+    result = [undo_take_stones(gameState, 2), undo_take_stones(gameState, 3)]
     none_filtering = [state for state in result if state is not None]
     # Filtering
-    if len(none_filtering)==0: # No previous states exist
+    if len(none_filtering) == 0:  # No previous states exist
         return []
     # Finding a match within a tree | NOTE: Have yet to find a case where a game state has 2 parents
     for level in tree:
         for item in none_filtering:
             if item in level:
                 return list(set(none_filtering) & set(level))
-    return [] # Failsafe
+    return []  # Failsafe
 
-def generateTree(startingGameState, height=-1): # Generate tree
+
+def generate_tree(startingGameState, height=-1):  # Generate tree
     # Initial starting value
-    game_tree=[[startingGameState]]
+    game_tree = [[startingGameState]]
     if height == -1:
-        height = getStones(startingGameState)//2 # Maximum height calculation
+        height = get_stones(startingGameState) // 2  # Maximum height calculation
     # Iterē pāri koka līmenim katram stāvoklim pievienojot nākamos stāvokļus
     for i in range(height):
         additional_level = []
         tree_level = game_tree[i]
         for gameState in tree_level:
-            additional_level += nextGameStates(gameState)
+            additional_level += next_game_states(gameState)
         # A failsafe
-        if (len(additional_level) == 0):
+        if len(additional_level) == 0:
             break
         # Duplikātu dzēšana
         additional_level = list(set(additional_level))
